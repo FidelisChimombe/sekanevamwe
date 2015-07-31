4e4a2fc85f082fac5e43ce4a8b2539aaa8355e65
Fidelis chimombe <fidelisgomaco@gmail.com>
2015-07-30 23:20:40 -0400

    whhyyyyyyyyyyyyyyyyyyyyyyyy

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ var helpers = require('express-helpers')(app); //automatically includes all view
 
 
 
-var connection_string = 'mongodb://localhost:28017/Sekanevamwe';
+var connection_string = 'mongodb://admin:password@localhost:28017/Sekanevamwe';
 
 // if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
 //         connection_string = 'mongodb://' + 
