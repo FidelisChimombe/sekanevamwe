7b3e8bda8eb7eaab511bb9501278f9e243d9cb0e
Fidelis chimombe <fidelisgomaco@gmail.com>
2015-07-30 22:34:19 -0400

    slowly getting there

--- app.js
+++ app.js
@@ -32,14 +32,14 @@ var helpers = require('express-helpers')(app); //automatically includes all view
 
 
 
-var connection_string = 'mongodb://localhost:27017/sekanevamwe';
+var connection_string = 'mongodb://localhost:27017/sekanevamwe-database';
 
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         connection_string = 'mongodb://' + 
-        process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':fidelis' +
+        process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/sekanevamwe';
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/sekanevamwe-database';
 }
 
 
