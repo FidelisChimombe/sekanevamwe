d258ac84192eeb4e47820f6563f520f7df03f10a
Fidelis chimombe <fidelisgomaco@gmail.com>
2015-08-01 16:05:15 -0400

    added mongolab

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ var helpers = require('express-helpers')(app); //automatically includes all view
 
 
 
-var connection_string = 'mongodb://localhost:27017/sekanevamwe';
+var connection_string =process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/sekanevamwe';
 
 // if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
 //         connection_string = 'mongodb://' + 
