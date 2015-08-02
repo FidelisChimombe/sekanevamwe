d18bc33726e060cc767c80bbfa8b983cdf9f62e7
Fidelis chimombe <fidelisgomaco@gmail.com>
2015-08-02 01:32:33 -0400

    solved the protocol problem

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
     var joke_id=joke_user_id[0];
     var user_id=joke_user_id[1];
     $.ajax({
-            url: "http://localhost:3000/jokes/like/"+joke_id,
+            url: "https://localhost:3000/jokes/like/"+joke_id,
             type: "POST",
             data:{ 
               
@@ -78,7 +78,7 @@ $(document).ready(function(){
       //   socket.emit('comment',{content:content,joke_id:joke_id,author_id:author_id,username:username});
       // }
       $.ajax({
-            url: "http://localhost:3000/comments/new_comment/",
+            url: "https://localhost:3000/comments/new_comment/",
             type: "POST",
             data:{ 
               "content" : content,
